3f4e45d1aed09f39e9ddeb041b1454f74ff368bc
Amha Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2023-02-03 01:53:54 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,7 +3,7 @@
 """
 A python module that solves a box puzzel.
 Puzzel:
-You have n number of locked boxes in front of you. 
+You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1 and
 each box may contain keys to the other boxes.
 

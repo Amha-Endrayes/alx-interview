72ca50a274a480bb279571749feb66fcd19b0e2a
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2023-02-08 01:47:45 +0300

    added answer script

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Given a number n, a module that calculates the fewest number
+of operations needed to result in exactly (n) 'H' characters
+in a text file.
+"""
+
+
+def minOperations(n):
+    """
+    Returns fewest number of opperations required to
+    duplicate a single character using two operations
+    (n) number of times.
+
+    Args:
+    @n - number of characters required
+
+    Returns:
+    int x - returns minimum number of operations
+    required to meet target if possible.
+    0 - if n is impossible to achieve
+
+    """
+    dp = [0] * (n + 1)
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        j = 2
+        while j <= i // 2:
+            if dp[j] + 1 < dp[i]:
+                dp[i] = dp[j] + 1
+            j += 1
+    return dp[n]

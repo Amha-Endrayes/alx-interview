2c5886e67ce5351058261d5a11abb83cc0e2ada0
Amha Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2023-02-16 21:41:59 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,59 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-a script that reads <stdin> line by line and computes metrics
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
 """
 import sys
-import signal
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-total_file_size = 0
-lines_by_status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
-# Define signal handler for CTRL + C
-def signal_handler(signal, frame):
-    print_stats()
-    sys.exit(0)
-
-# Register signal handler for CTRL + C
-signal.signal(signal.SIGINT, signal_handler)
-
-# Function to parse the log line and update the metrics
-def parse_log_line(line):
-    global total_file_size
-    global lines_by_status_code
-    global line_count
-
-    # Parse log line
+if __name__ == "__main__":
+    cnt = 0
     try:
-        ip, date, request, status_code, file_size = line.split(" ")
-        if request != "GET /projects/260 HTTP/1.1":
-            return
-        status_code = int(status_code)
-        file_size = int(file_size)
-    except ValueError:
-        return
-
-    # Update metrics
-    total_file_size += file_size
-    lines_by_status_code[status_code] += 1
-    line_count += 1
-
-    # Print stats if we've processed 10 lines
-    if line_count % 10 == 0:
-        print_stats()
-
-# Function to print the current statistics
-def print_stats():
-    print(f"Total file size: {total_file_size}")
-    for status_code in sorted(lines_by_status_code.keys()):
-        if lines_by_status_code[status_code] > 0:
-            print(f"{status_code}: {lines_by_status_code[status_code]}")
-
-# Read input from stdin line by line
-for line in sys.stdin:
-    parse_log_line(line)
-
-    # Print stats if we've processed 10 lines
-    if line_count % 10 == 0:
-        print_stats()
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
+    except KeyboardInterrupt:
+        prn_stats()
+        raise
+    else:
+        prn_stats()

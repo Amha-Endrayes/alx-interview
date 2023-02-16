1c9c3b9ed88fa6c4d11f64bff8fc0cd2ef5b4953
Amha Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2023-02-16 21:47:17 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,30 +3,20 @@
 Script that reads stdin line by line and computes metrics:
 - Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
                 code> <file size>
-- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-    statistics from the beginning:
-Example:
-    File size: 5213
-    200: 2
-    401: 1
-    403: 2
-    404: 1
-    405: 1
-    500: 3
 """
 import sys
 stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
         "403": 0, "404": 0, "405": 0, "500": 0}
-summ = 0
+total = 0
 
 
-def prn_stats():
+def print_stats():
     """
     Function that print stats about log
     """
-    global summ
+    global total
 
-    print('File size: {}'.format(summ))
+    print('File size: {}'.format(total))
     stcdor = sorted(stcd.keys())
     for each in stcdor:
         if stcd[each] > 0:
@@ -34,7 +24,7 @@ def prn_stats():
 
 
 if __name__ == "__main__":
-    cnt = 0
+    count = 0
     try:
         """ Iter the standar input """
         for data in sys.stdin:
@@ -44,16 +34,16 @@ if __name__ == "__main__":
                 if fact[-2] in stcd:
                     stcd[fact[-2]] += 1
                 """ If there is a lenght """
-                summ += int(fact[-1])
+                total += int(fact[-1])
             except:
                 pass
             """ Printing control """
-            cnt += 1
-            if cnt == 10:
-                prn_stats()
-                cnt = 0
+            count += 1
+            if count == 10:
+                print_stats()
+                count = 0
     except KeyboardInterrupt:
-        prn_stats()
+        print_stats()
         raise
     else:
-        prn_stats()
+        print_stats()
